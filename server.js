96713ef6fca6e77bb6d420f8a4e3c696a02d1f4d
xvirix <77359420+xvirix@users.noreply.github.com>
2021-05-07 21:39:52 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+
+//Build Server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Server middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
