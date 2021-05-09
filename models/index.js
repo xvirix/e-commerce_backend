b92d64c3ef2c943a22621acfdc56e101430d16ff
xvirix <77359420+xvirix@users.noreply.github.com>
2021-05-08 23:46:52 -0500

    Updated

--- models/index.js
+++ models/index.js
@@ -17,14 +17,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "tagged_products",
   foreignKey: "product_id"
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "tagged_products",
   foreignKey: "tag_id"
 });
 
